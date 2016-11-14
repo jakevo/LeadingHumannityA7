148c7663d7d0196d414024e673d2834025bb9fd7
Alejandro Panduro <apanduro@ucsd.edu>
2016-11-14 13:35:44 -0800

    updated

--- create.js
+++ create.js
@@ -1,19 +1,36 @@
 
-var projectName;
+var array1 = [];
+var pName;
 var topic;
 var quest;
 var hypo;
 var url;
-var object = new Object();
-
 
 function func1(a,b,c,d,e) {
 
-    projectName = a;
+    pName = a;
     topic = b;
     ques = c;
     hypo = d;
     url = e;
-
-
 };
+
+function getProjectName() {
+  return pName;
+}
+
+function getTopicName() {
+  return topic;
+}
+
+function getQuestion() {
+  return ques;
+}
+
+function getHypo() {
+  return hypo;
+}
+
+function getUrl() {
+  return url;
+}
