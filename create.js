f038e3f48977c62cec9912057ebf7c6b7315adbe
Jake Vo <javo@ucsd.edu>
2016-11-14 13:30:30 -0800

    code for A7

---
+++ create.js
@@ -0,0 +1,19 @@
+
+var projectName;
+var topic;
+var quest;
+var hypo;
+var url;
+var object = new Object();
+
+
+function func1(a,b,c,d,e) {
+
+    projectName = a;
+    topic = b;
+    ques = c;
+    hypo = d;
+    url = e;
+
+
+};
